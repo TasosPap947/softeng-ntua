2df0c6881723c9897133dfe09cebd6247cc3b00b
WinRout <nikitsin2000@gmail.com>
2022-01-04 15:03:28 +0200

    Implemented endpoint functions
    PassesAnalysis.js and PassesPerStation.js. Also updated packaje.json
    with new depedencies required ("moment": "^2.29.1", "util": "^0.12.4").

--- api/endpoints/operational/PassesAnalysis.js
+++ api/endpoints/operational/PassesAnalysis.js
@@ -1,14 +1,77 @@
 const express = require("express");
 const router = express.Router();
+const mysql = require("mysql");
+const util = require("util");
+const moment = require('moment');
 
-function PassesAnalysis(req, res) {
-  res.send({
-    function: "PassesAnalysis",
-    op1_ID: req.params.op1_ID,
-    op2_ID: req.params.op2_ID,
-    date_from: req.params.date_from,
-    date_to: req.params.date_to,
-  });
+const response = require("../../utilities/FormattedResponse.js");
+const { conString } = require("../../utilities/definitions.js");
+
+async function PassesAnalysis(req, res) {
+
+  const { op1_ID, op2_ID, date_from, date_to } = req.params;
+  const format = req.query.format;
+
+  const dateFormat = 'YYYY-MM-DD HH:mm:ss';
+  const currentTimestamp = moment().format(dateFormat);
+
+  //PassesList query
+  const PassesListQuery = `
+  SELECT
+    p.passID AS PassID,
+    p.StationstationID AS StationID,
+    p.DateAndTime AS TimeStamp,
+    p.VehiclevehicleID AS VehicleID,
+    p.charge AS Charge
+  FROM
+    Passes p JOIN Vehicle v ON (p.VehiclevehicleID = v.vehicleID AND
+    ? = v.tagProvider)
+  WHERE
+		? = (SELECT s.stationProvider FROM Station s WHERE p.StationstationID = s.stationID) AND
+        DateAndTime BETWEEN ? AND ?
+        ORDER BY p.DateAndTime ASC`;
+
+  try {
+    //create connection with database
+    const conn = await mysql.createConnection(conString);
+    const query = util.promisify(conn.query).bind(conn);
+
+    //do query for PassesList
+    const PassesListRes = await query(PassesListQuery, [
+      op2_ID,
+      op1_ID,
+      date_from,
+      date_to
+    ]);
+    //if no result given send error message
+    if (!PassesListRes[0]) {
+      response(res, 402, {message: "No available data for specified " +
+      "providers and time period."});
+    }
+    //Parse result as JS Object and compute total length
+    let PassesList = JSON.parse(JSON.stringify(PassesListRes));
+    let i = 0;
+    PassesList.forEach((pass) => {
+        pass.PassIndex = ++i;
+    });
+
+    //send final response
+    response(res, 200, {
+      op1_ID: op1_ID,
+      op2_ID: op2_ID,
+      RequestTimestamp: currentTimestamp,
+      PeriodFrom: date_from,
+      PeriodTo: date_to,
+      NumberOfPasses: i,
+      PassesList: PassesList
+    }, format);
+
+    conn.end();
+
+  } catch (err) {
+    response(res, 500, {message: 'Internal server error', error: String(err)}, format);
+    conn.end();
+  }
 }
 
 router.get(
