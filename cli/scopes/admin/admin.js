d97ded2b69ff4adc02865f1712c43e2e241eca17
nikpag00 <nikospagonas00@gmail.com>
2022-01-13 13:00:51 +0200

    Reset admin.js functionality
    admin.js functionality (updpasses) won't be called by the API, so it will be implemented
    inside of the cli.

--- cli/scopes/admin/admin.js
+++ cli/scopes/admin/admin.js
@@ -2,10 +2,7 @@ const axios = require("axios");
 const env = require("../../env.js");
 
 function adminFunction(source) {
-    /* give a name to the url */
-    axios.post(`${env.baseURL}/admin/.....`).then((res) => {
-        console.log(res.data);
-    });
+    /* not implemented yet */
 }
 
 module.exports = { adminFunction };
