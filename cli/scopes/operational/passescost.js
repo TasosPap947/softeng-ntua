74ff6cacb7a7be511cd98ddfa40d5c4e46c3bc62
nikpag00 <nikospagonas00@gmail.com>
2022-01-14 21:33:13 +0200

    Make cli accept format parameter

--- cli/scopes/operational/passescost.js
+++ cli/scopes/operational/passescost.js
@@ -1,10 +1,12 @@
 const axios = require("axios");
 const env = require("../../env.js");
 
-function passescostFunction(op1_ID, op2_ID, date_from, date_to) {
-    env.instance.get(`${env.baseURL}/PassesCost/${op1_ID}/${op2_ID}/${date_from}/${date_to}`).then((res) => {
-        console.log(res.data);
-    });
+function passescostFunction(op1_ID, op2_ID, date_from, date_to, format) {
+    env.instance
+        .get(`${env.baseURL}/PassesCost/${op1_ID}/${op2_ID}/${date_from}/${date_to}?format=${format}`)
+        .then((res) => {
+            console.log(res.data);
+        });
 }
 
 module.exports = { passescostFunction };
--- cli/scopes/operational/passesperstation.js
+++ cli/scopes/operational/passesperstation.js
@@ -1,10 +1,12 @@
 const axios = require("axios");
 const env = require("../../env.js");
 
-function passesperstationFunction(stationID, date_from, date_to) {
-    env.instance.get(`${env.baseURL}/PassesPerStation/${stationID}/${date_from}/${date_to}`).then((res) => {
-        console.log(res.data);
-    });
+function passesperstationFunction(stationID, date_from, date_to, format) {
+    env.instance
+        .get(`${env.baseURL}/PassesPerStation/${stationID}/${date_from}/${date_to}?format=${format}`)
+        .then((res) => {
+            console.log(res.data);
+        });
 }
 
 module.exports = { passesperstationFunction };
