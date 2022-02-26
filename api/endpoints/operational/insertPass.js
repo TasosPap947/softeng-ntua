f7ebfd6c8262da708485241c5a87e4ba9dcc9938
nikpag00 <nikospagonas00@gmail.com>
2022-02-26 19:10:58 +0200

    Fix small API bugs

--- api/endpoints/operational/insertPass.js
+++ api/endpoints/operational/insertPass.js
@@ -2,34 +2,31 @@ const express = require("express");
 const router = express.Router();
 const mysql = require("mysql");
 const util = require("util");
-const moment = require('moment');
+const moment = require("moment");
 
 const response = require("../../utilities/responseFunctions.js");
 const { conString } = require("../../utilities/definitions.js");
 
 function makePassID() {
-    var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    var numbers = '0123456789';
+    var result = "";
+    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numbers = "0123456789";
     var charactersLength = characters.length;
     var numbersLength = numbers.length;
     for (var i = 0; i < 3; i++) {
-        result += characters.charAt(Math.floor(Math.random() *
-            charactersLength));
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     for (var i = 0; i < 7; i++) {
-        result += numbers.charAt(Math.floor(Math.random() *
-            numbersLength));
+        result += numbers.charAt(Math.floor(Math.random() * numbersLength));
     }
     return result;
 }
 
 async function insertPass(req, res) {
-
     const { charge, vehicleID, stationID } = req.params;
     const format = req.query.format;
 
-    const dateFormat = 'YYYY-MM-DD HH:mm:ss';
+    const dateFormat = "YYYY-MM-DD HH:mm:ss";
     const currentTimestamp = moment().format(dateFormat);
 
     const insertQuery = `INSERT INTO \`Passes\` VALUES (?, ?, ?, ?, ?);`;
@@ -37,31 +34,30 @@ async function insertPass(req, res) {
     var duplicate = true;
 
     const passID = makePassID();
+    let conn;
     try {
-        const conn = await mysql.createConnection(conString);
+        conn = await mysql.createConnection(conString);
         const query = util.promisify(conn.query).bind(conn);
-        const result = await query(insertQuery, [
-            passID,
-            currentTimestamp,
-            charge,
-            vehicleID,
-            stationID
-        ]);
-        response.general(res, 200, {
-            status: "OK",
-            RequestTimestamp: currentTimestamp,
-            ItemInserted: {
-                passID: passID,
-                DateAndTime: currentTimestamp,
-                charge: charge,
-                VehiclevehicleID: vehicleID,
-                StationstationID: stationID
-            }
-        }, format);
+        const result = await query(insertQuery, [passID, currentTimestamp, charge, vehicleID, stationID]);
+        response.general(
+            res,
+            200,
+            {
+                status: "OK",
+                RequestTimestamp: currentTimestamp,
+                ItemInserted: {
+                    passID: passID,
+                    DateAndTime: currentTimestamp,
+                    charge: charge,
+                    VehiclevehicleID: vehicleID,
+                    StationstationID: stationID,
+                },
+            },
+            format
+        );
         conn.end();
-    }
-    catch (err) {
-        response.general(res, 500, { status: failed, message: 'Internal server error', error: String(err) }, format);
+    } catch (err) {
+        response.general(res, 500, { status: "failed", message: "Internal server error", error: String(err) }, format);
         conn.end();
     }
 }
--- api/endpoints/operational/operatorList.js
+++ api/endpoints/operational/operatorList.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mysql = require("mysql");
 const util = require("util");
-const moment = require('moment');
+const moment = require("moment");
 
 const response = require("../../utilities/responseFunctions.js");
 const { conString } = require("../../utilities/definitions.js");
@@ -10,10 +10,9 @@ const { conString } = require("../../utilities/definitions.js");
 const { host, port, baseURL } = require("../../utilities/definitions.js");
 
 async function operatorList(req, res) {
-
     const format = req.query.format;
 
-    const dateFormat = 'YYYY-MM-DD HH:mm:ss';
+    const dateFormat = "YYYY-MM-DD HH:mm:ss";
     const currentTimestamp = moment().format(dateFormat);
 
     const OperatorQuery = `
@@ -29,17 +28,18 @@ async function operatorList(req, res) {
 
         const OperatorRes = await query(OperatorQuery);
 
-        response.general(res, 500, {
-            RequestTimestamp: currentTimestamp,
-            NumberOfOperators: OperatorRes.length,
-            operators: OperatorRes
-        }, format);
-
-
-        
-    }
-    catch (err) {
-        response.general(res, 500, { message: 'Internal server error', error: String(err) }, format);
+        response.general(
+            res,
+            200,
+            {
+                RequestTimestamp: currentTimestamp,
+                NumberOfOperators: OperatorRes.length,
+                operators: OperatorRes,
+            },
+            format
+        );
+    } catch (err) {
+        response.general(res, 500, { message: "Internal server error", error: String(err) }, format);
         conn.end();
     }
 }
